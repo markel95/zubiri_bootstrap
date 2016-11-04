c1b076662ca4f9cc0cc0b6e4e55a0b18dc777ac2
markel95 <markelarra95@gmail.com>
2016-11-04 12:00:11 +0000

    04/11/2016 12:59

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function (){
+
+$('a[href*="#"]:not([href="#"])').click(function() {
+    
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
+// create a LatLng object containing the coordinate for the center of the map
+var latlng = new google.maps.LatLng(-33.86455, 151.209);
+// prepare the map properties
+var options = {
+zoom: 15,
+center: latlng,
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+navigationControl: true,
+mapTypeControl: false,
+scrollwheel: false,
+disableDoubleClickZoom: true
+};
+// initialize the map object
+var map = new google.maps.Map(document.getElementById('google_map'), options);
+// add Marker
+var marker1 = new google.maps.Marker({
+position: latlng, map: map
+});
+// add listener for a click on the pin
+google.maps.event.addListener(marker1, 'click', function() {
+infowindow.open(map, marker1);
+});
+// add information window
+var infowindow = new google.maps.InfoWindow({
+content:  '<div class="info"><strong>This is my company</strong><br><br>My company address is here<br> 32846 Sydney</div>'
+});  
+});
